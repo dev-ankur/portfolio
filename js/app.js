6223a7814a177bc9bfb43b7ae0823e72f51d41c5
Ankur Shah <ankur3291@gmail.com>
2017-01-07 12:52:25 +0530

    initial commit local

---
+++ js/app.js
@@ -0,0 +1,4 @@
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*
+	var anim = "#particles-js .pro-title";
+	var events = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+	$(anim).one(events, function(){
+		$(anim).removeClass("animated zoomIn");
+		$(anim).addClass("animated bounceIn");
+	})
+*/
+});
